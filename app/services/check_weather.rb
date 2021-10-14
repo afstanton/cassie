0254a3baa50225c3e6e1ae666fd9a3a8c24cf262
Aaron F Stanton <afstanton@gmail.com>
2021-10-13 21:11:43 -0400

    Check weather service

---
+++ app/services/check_weather.rb
@@ -0,0 +1,21 @@
+class CheckWeather
+  attr_accessor :client
+
+  def initialize
+    @client = OpenWeather::Client.new(api_key: ENV['OPEN_WEATHER'])
+  end
+
+  def current(zip)
+    @client.current_zip(zip)
+  end
+
+  def check(zip)
+    weather = Weather.find_or_initialize_by(zip: zip)
+    if weather.new_record? || weather.updated_at < 30.minutes.ago
+      weather.data = current(zip)
+      weather.save
+    end
+
+    weather.data
+  end
+end
--- db/migrate/20211014010256_create_weathers.rb
+++ db/migrate/20211014010256_create_weathers.rb
@@ -1,8 +1,8 @@
 class CreateWeathers < ActiveRecord::Migration[6.1]
   def change
     create_table :weathers do |t|
-      t.string :zip
-      t.jsonb :data
+      t.string :zip, null: false
+      t.jsonb :data, null: false
 
       t.timestamps
     end
