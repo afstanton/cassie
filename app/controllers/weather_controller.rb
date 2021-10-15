88dcf3e4bf0cddb48142c64de55b87c1054e3732
Aaron F Stanton <afstanton@gmail.com>
2021-10-14 22:04:45 -0400

    Some edge cases

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -3,12 +3,14 @@
 class WeatherController < ApplicationController
   def index
     @original_address = address_params['address']
-    verifier = MainStreet::AddressVerifier.new(@original_address)
-    if verifier.success?
-      @weather, @cached = CheckWeather.check(verifier.result.postal_code)
-      @verified_address = verifier.result.address
-    else
-      @error_message = verifier.failure_message
+    if @original_address.present?
+      verifier = MainStreet::AddressVerifier.new(@original_address)
+      if verifier.success? && verifier.result.postal_code.present?
+        @weather, @cached = CheckWeather.check(verifier.result.postal_code)
+        @verified_address = verifier.result.address
+      else
+        @error_message = verifier.failure_message.present? ? verifier.failure_message : 'Zip code not found'
+      end
     end
   end
 
